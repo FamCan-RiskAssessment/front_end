e59701ec77c7a5566db7106ae1ea11a66f081660
Amir Akbari <aaryai473@gmail.com>
2025-09-15 16:37:56 +0330

    nulls are malls

--- src/radio.jsx
+++ src/radio.jsx
@@ -31,8 +31,8 @@ function Radio({data , class_change1 , class_change2 , valueSetter}){
                 {opt == "خیر" && data.options.length != 4 && (
                 <input type="radio" className="radio" name={data.Rname} value={false} id={opt} onChange={(e) => valueSetter(e.target.id)} />
                 )}
-                {opt != "بله" && opt != "خیر" && data.options.length != 4 && (
-                <input type="radio" className="radio" name={data.Rname} value={null} id={opt} onChange={(e) => valueSetter(e.target.value)} />
+                {opt != "خیر" && opt != "بله" && data.options.length != 4 && (
+                <input type="radio" className="radio" name={data.Rname} value="null" id={opt} onChange={(e) => valueSetter(e.target.id)} />
                 )}
                 {data.options.length >= 4 && (
                 <input type="radio" className="radio" name={data.Rname} value={opt} id={opt} onChange={(e) => valueSetter(e.target.id)} />
