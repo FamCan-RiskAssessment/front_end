b60c8768025ce9ba4b8b4983c95ee17c63df73d9
Amir Akbari <aaryai473@gmail.com>
2026-02-16 13:25:27 +0330

    rangeBox bug fixed

--- src/rangeInp.jsx
+++ src/rangeInp.jsx
@@ -20,7 +20,8 @@ function RangeBox({ data_req, data, valueSetter, class_change1, class_change2, r
                     <label htmlFor={data.engName}>{data.inpName}</label>
                     <input data_req={data_req} type="range" name={data.engName} value={vol} onChange={(e) => setVol(e.target.value)} max={data.lim != undefined ? data.lim : 99} min={0} step={1} defaultValue={0} className={class_change2} />
                     <div className="number">
-                        <span className="increase R" onClick={() => setVol(v => {
+                        <span className="increase R" onClick={() => setVol(vo => {
+                            let v = Number(vo)
                             if (data.lim && v == data.lim) {
                                 return v
                             } else {
@@ -34,7 +35,8 @@ function RangeBox({ data_req, data, valueSetter, class_change1, class_change2, r
                             <img src={plusSign} alt="plusSign" />
                         </span>
                         <span className="self_number">عدد انتخابی :  {vol}</span>
-                        <span className="decrease R" onClick={() => setVol(v => {
+                        <span className="decrease R" onClick={() => setVol(vo => {
+                            let v = Number(vo)
                             if (v == 0) {
                                 return v
                             } else {
